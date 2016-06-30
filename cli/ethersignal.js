72a1c1af9be4d3bf9294425a01f4159df010c369
kor <kor@localhost.localdomain>
2016-06-29 19:46:24 -0700

    only call getBalance once per acct and use a function callback instead of  get

--- cli/ethersignal.js
+++ cli/ethersignal.js
@@ -2,25 +2,31 @@ var esContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"proposa
 var ethersignal = esContract.at('0x88d97abc052ae9ac3f2d034590265ec381f70a9b')
 
 function CalcSignal(proposalHash) {
-	var sigEvs = ethersignal.LogSignal({proposalHash: proposalHash}, {fromBlock: 1200000}).get()
-
 	var proMap = {};
 	var antiMap = {};
-	console.log("sigEvs.length: " + sigEvs.length);
-	for(var i = 0; i < sigEvs.length; i++) {
-		var bal = web3.fromWei(web3.eth.getBalance(sigEvs[i].args.addr));
 
-		if (sigEvs[i].args.pro) {
-			proMap[sigEvs[i].args.addr] = bal;
-			antiMap[sigEvs[i].args.addr] = 0;
-		} else {
-			proMap[sigEvs[i].args.addr] = 0;
-			antiMap[sigEvs[i].args.addr] = bal;
+	ethersignal.LogSignal({proposalHash: proposalHash}, {fromBlock: 1200000}, function(error, result){
+		if (!error)
+		{
+			if (result.args.pro) {
+				proMap[result.args.addr] = 1;
+				antiMap[result.args.addr] = 0;
+			} else {
+				proMap[result.args.addr] = 0;
+				antiMap[result.args.addr] = 1;
+			}
 		}
-	}
+	})
 
 	var totalPro = 0;
 	var totalAgainst = 0;
+
+	Object.keys(proMap).map(function(a) {
+		var bal = web3.fromWei(web3.eth.getBalance(a));
+		proMap[a] = proMap[a] * bal;
+		antiMap[a] = antiMap[a] * bal;
+	});
+
 	Object.keys(proMap).map(function(a) { totalPro += parseFloat(proMap[a]); });
 	Object.keys(antiMap).map(function(a) { totalAgainst += parseFloat(antiMap[a]); });
 
