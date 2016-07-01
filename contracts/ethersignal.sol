f456420af46ed4fb84351180e1fb8deebc52ad7b
kor <kor@localhost.localdomain>
2016-07-01 15:59:17 -0700

    commit with burn at 4096 iters

--- contracts/ethersignal.sol
+++ contracts/ethersignal.sol
@@ -1,7 +1,7 @@
 contract EtherSignal {
 	event LogSignal(bytes32 indexed proposalHash, bool pro, address addr);
 	function setSignal(bytes32 proposalHash, bool pro) {
-		for (uint i = 0; i < 1024; i++) { } // burn some gas to avoid event spamming
+		for (uint i = 0; i < 4096; i++) { } // burn some gas to increase DOS cost
 		LogSignal(proposalHash, pro, msg.sender);
     }
 }
