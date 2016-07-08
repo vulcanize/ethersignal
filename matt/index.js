2871e867aa2f21da7e675a80e9d0b9d9c12f9793
matthewnolan <matthewnolan415@gmail.com>
2016-07-08 15:51:31 -0400

    index

--- matt/index.js
+++ matt/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
+
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
