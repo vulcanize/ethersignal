4c54f2af563349f529931a98717f017fe6850f72
Mohamad Balaa <buddybalaa@gmail.com>
2016-07-11 20:45:40 -0700

    From branch dev commit 8759510e519d48fc77706c7424353b01331289ca

--- README2.md
+++
@@ -1,4 +0,0 @@
-# Ether Signal
-
-Contract on Testnet:
-http://testnet.etherscan.io/address/0x851a78f09511bf510ad27036f5ff7c8901fdd2e2#code
--- cli/test1.js
+++
@@ -1,5 +0,0 @@
-function test1_voteSpam() {
-	for (i = 0; i < 100; i++) {
-		ethersignal.setSignal(0, ((i % 2) == 0) ? true : false, {from:  web3.eth.accounts[0], gas: 300000});
-	}
-}
