4dfa6ef13e84d963f693c86e6ca5ba1be8c8f19c
Blaine Price <1wbprice@gmail.com>
2016-07-27 12:37:52 -0400

    rough out structure for atomic components.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
+import App from './components/environments/App'
 import './index.css'
 
 ReactDOM.render(
